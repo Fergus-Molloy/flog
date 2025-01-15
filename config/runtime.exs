3603579238cff2e9958429ab5c1dfd9ab8b1450e
Fergus Molloy <fergus@molloy.xyz>
2025-01-15 11:19:02 +0000

    use || to set default for ip log path

--- config/runtime.exs
+++ config/runtime.exs
@@ -65,14 +65,7 @@ if config_env() == :prod do
     ],
     secret_key_base: secret_key_base
 
-  ip_log_path =
-    case System.get_env("IP_LOG_PATH", nil) do
-      # disable access log if env var not set
-      nil -> "/dev/null"
-      path -> path
-    end
-
-  config :flog, FlogWeb.IpLogger, log_path: ip_log_path
+  config :flog, FlogWeb.IpLogger, log_path: System.get_env("IP_LOG_PATH") || "/dev/null"
 
   # ## SSL Support
   #
